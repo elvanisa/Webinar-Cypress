9010895079ab1db58ef5aff52541e193f896df3d
elvanisa <elvanisamuhsina@gmail.com>
2023-09-07 16:20:11 +0700

    first commit

---
+++ cypress/e2e/file1.cy.js
@@ -0,0 +1,17 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
+  it('success login', () => {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.title').should('have.text', 'Products')
+  })
+  it('failed login - wrong credentials', () => {
+    cy.get('[data-test="username"]').should('be.visible').type('salah user')
+    cy.get('[data-test="password"]').should('be.visible').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('contain.text', 'do not match')
+  })
+})
