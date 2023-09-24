409ac2715e2f82e44edfe9cc13e7d6f1eca24c65
elvanisaa <elvanisa@ayp-group.com>
2023-09-24 14:00:08 +0700

    update name

--- cypress/e2e/file1.cy.js
+++ cypress/e2e/file1.cy.js
@@ -3,8 +3,8 @@ describe('template spec', () => {
     cy.visit('https://www.saucedemo.com/')
   })
   it('success login', () => {
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="username"]').should('be.visible').type('standard_user')
+    cy.get('[data-test="password"]').should('be.visible').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
     cy.get('.title').should('have.text', 'Products')
   })
